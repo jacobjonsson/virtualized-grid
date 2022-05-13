f6bf00e6242c5df2459e66f0b4e100ffeeea432d
Jacob Jonsson <jacobjjonsson@gmail.com>
2022-05-13 12:20:45 +0200

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,84 @@
+import { faker } from "@faker-js/faker";
+import { FixedSizeGrid } from "react-window";
+import { useRect } from "@reach/rect";
+import { Card } from "./card";
+import { forwardRef } from "react";
+import { useRef } from "react";
+
+function createItem() {
+  return {
+    id: Math.random().toString(),
+    image: "https://picsum.photos/400",
+    title: faker.word.adjective(),
+    subTitle: faker.date.recent().toLocaleDateString(),
+  };
+}
+
+const items = Array(10000).fill(null).map(createItem);
+
+const GUTTER_SIZE = 16;
+const MIN_WIDTH = 224;
+
+export function App() {
+  const containerRef = useRef(null);
+  const rect = useRect(containerRef, { observe: true });
+
+  function getColumnCount(width) {
+    const columnCount = Math.floor(width / (GUTTER_SIZE + MIN_WIDTH));
+    const whiteSpace = width - columnCount * (GUTTER_SIZE + MIN_WIDTH);
+    const spaceToAdd = whiteSpace / columnCount;
+    return { columnCount, columnWidth: MIN_WIDTH + spaceToAdd };
+  }
+
+  const width = rect?.width - 16 || 0;
+  const height = rect?.height || 0;
+  const { columnCount, columnWidth } = getColumnCount(width);
+  const rowCount = Math.ceil(items.length / columnCount);
+  const rowHeight = columnWidth * 0.75;
+
+  return (
+    <div style={{ height: "100%", width: "100%" }} ref={containerRef}>
+      <FixedSizeGrid
+        width={width}
+        height={height}
+        columnCount={columnCount}
+        columnWidth={columnWidth + GUTTER_SIZE}
+        rowCount={rowCount}
+        rowHeight={rowHeight + GUTTER_SIZE}
+        innerElementType={InnerElementType}
+      >
+        {({ columnIndex, rowIndex, style }) => {
+          const item = items[rowIndex * columnCount + columnIndex];
+          return (
+            <Card
+              style={{
+                ...style,
+                left: style.left + GUTTER_SIZE,
+                top: style.top + GUTTER_SIZE,
+                width: style.width - GUTTER_SIZE,
+                height: style.height - GUTTER_SIZE,
+              }}
+              {...item}
+            />
+          );
+        }}
+      </FixedSizeGrid>
+    </div>
+  );
+}
+
+const InnerElementType = forwardRef(({ style, ...rest }, ref) => {
+  return (
+    <div
+      ref={ref}
+      style={{
+        ...style,
+        paddingLeft: GUTTER_SIZE,
+        paddingTop: GUTTER_SIZE,
+        paddingRight: GUTTER_SIZE,
+        paddingBottom: GUTTER_SIZE,
+      }}
+      {...rest}
+    />
+  );
+});
---
+++ src/card.jsx
@@ -0,0 +1,83 @@
+import styled from "styled-components";
+import * as ContextMenu from "@radix-ui/react-context-menu";
+
+export function Card({ style, image, title, subTitle }) {
+  return (
+    <ContextMenu.Root>
+      <ContextMenu.Trigger asChild>
+        <Container style={style}>
+          <ImageContainer>
+            <Image src={image} />
+          </ImageContainer>
+          <InfoContainer>
+            <Title>{title}</Title>
+            <SubTitle>{subTitle}</SubTitle>
+          </InfoContainer>
+        </Container>
+      </ContextMenu.Trigger>
+      <ContextMenuContent>
+        <ContextMenuItem>Rename</ContextMenuItem>
+        <ContextMenuItem>Duplicate</ContextMenuItem>
+        <ContextMenuItem>Delete</ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu.Root>
+  );
+}
+
+const Container = styled.div`
+  height: ${(props) => props.$height}px;
+  width: ${(props) => props.$width}px;
+  display: flex;
+  flex-direction: column;
+  background-color: #3f3f46;
+  border-radius: 4px;
+  padding: 8px;
+`;
+
+const ImageContainer = styled.div`
+  height: 100%;
+  flex-shrink: 1;
+  flex-grow: 1;
+  overflow: hidden;
+`;
+
+const Image = styled.img`
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+  flex-shrink: 0;
+  flex-grow: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+const Title = styled.span`
+  font-size: 14px;
+  color: #fff;
+`;
+
+const SubTitle = styled.span`
+  font-size: 12px;
+  color: rgb(160, 174, 192);
+`;
+
+const ContextMenuContent = styled(ContextMenu.Content)`
+  padding: 8px 8px;
+  border-radius: 6px;
+  background-color: #fff;
+  border: 1px solid #e2e8f0;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const ContextMenuItem = styled(ContextMenu.Item)`
+  cursor: pointer;
+  font-size: 12px;
+  padding: 8px;
+`;
